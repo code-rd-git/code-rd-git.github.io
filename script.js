20bc21d7dc44f6ffefb0caab916c7c29b25cebe2
Romain David <rd@RD-PC.cei.local>
2024-12-02 08:41:51 +0100

    Add resource folder to display data from json file

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var main_container = document.getElementById('main')
+const HTML_TECH = document.getElementById('tech')
 
-function jumpTo(_tag)
-{
-    const tag_y_position = document.getElementById(_tag).offsetTop
-    main_container.scrollTo(tag_y_position, top)
-}
+if (HTML_TECH) fetch('./resource/technologies.json')
+    .then(response => response.json())
+    .then(data => {
+        let parsed = ''
+        for (item of data) parsed += `
+            <a class="card" href="${item.link}" target="_blank" title="Click to access ${item.title} documentation">
+                <img src="${item.image}"/>
+                <span>${item.title}</span>
+            </a>
+        `
+        HTML_TECH.innerHTML = parsed
+    })
